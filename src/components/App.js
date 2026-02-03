dcaa8d0b3c2e84818c625e996ef97d8746c0b4eb
Kartik <Kartiknoriya56@gmail.com>
2026-02-03 18:12:15 +0530

    functionality complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
+
+
+
 function App() {
+  const [showpara, setShowpara] = useState(true);
+  const [btntoggle , setBtntoggle] = useState(false);
+
+
+  function handleToggle(){
+    setBtntoggle ((prev) => !prev)   
+    setShowpara ((prev) => !prev) 
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleToggle}>
+        {btntoggle ? 'Hide text' : 'show text'}
+      </button>
+      <p>{showpara ? ' ' : `Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.`}</p>
+      {showpara}
     </div>
   );
 }
-
 
 export default App;
